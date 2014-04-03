193ddad5dabf3f274c54768d656c9d8a4fbd0398
JDAM3 <andre.bernecker@student.uibk.ac.at>
2014-04-03 12:09:33 +0200

    interfaces implementiert

--- src/com/co2mpare/RouteCalc/GoogleMap.java
+++ src/com/co2mpare/RouteCalc/GoogleMap.java
@@ -1,5 +1,20 @@
 package com.co2mpare.RouteCalc;
 
-public class GoogleMap {
+import com.co2mpare.interfaces.GMinterface;
+import com.google.android.gms.maps.model.LatLng;
+
+public class GoogleMap implements GMinterface{
+
+	@Override
+	public RouteGM getRoute(String src, String dest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RouteGM getRoute(LatLng src, LatLng dest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
